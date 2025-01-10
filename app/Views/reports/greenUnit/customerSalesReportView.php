4966051aaf45a18d3106c21726748cdc436330b5
IsaacWasukira <isaacwasukira@gmail.com>
2025-01-11 01:04:55 +0300

    Working on customer sales

--- app/Views/reports/greenUnit/customerSalesReportView.php
+++ app/Views/reports/greenUnit/customerSalesReportView.php
@@ -6,59 +6,56 @@
 <div class="az-content-body">
   <br>
   <div class="card bd-0">
-    <!-- <div class="card-header bg-gray-400 bd-b-0-f pd-b-0" style="background-color: green;">
-      <nav class="nav nav-tabs">
-        <a class="nav-link" data-toggle="tab" href="#buyersTab" style="background-color:firebrick; color: white">Sales Reports</a>
-      </nav>
-    </div>card-header -->
+    <h4>Customer Sales</h4>
     <div class="card-body bd bd-t-0 tab-content">
-      <div id="summary" class="tab-pane active show">
-        <div class="row">
-          <h4>Customer Sales</h4>
+      <div class="row">
+        <div class="col-sm-5 col-md-4">
+          <!-- <div class="row"> -->
+          <div class="form-group mb-2">
+            <label for="reportCustomer" class="az-content-label tx-11 tx-medium tx-gray-600">Customer:</label>
+            <input id="reportCustomer" class="form-control form-control-sm" style="width: 400px">
+          </div>
+          <!-- </div> -->
         </div>
-        <div class="row">
-          <div class="col-sm-6 col-md-4">
-            <label><strong>Select Customer:</strong></label>
-            <div class="row mg-t-10">
-              <div class="col-lg-3">
-                <label class="rdiobox">
-                  <input name="customerFilter" type="radio" value="all" checked="">
-                  <span>All</span>
-                </label>
-              </div><!-- col-3 -->
-              <div class="col-lg-3 mg-t-20 mg-lg-t-0">
-                <label class="rdiobox">
-                  <input name="customerFilter" type="radio" value="specific">
-                  <span>Specific</span>
-                </label>
-              </div><!-- col-3 -->
-            </div>
-            <div class="row">
+        <div class="col-sm-5 col-md-3">
+          <div class="row">
+            <div class="col-sm-5">
               <div class="form-group mb-2">
-                <label class="az-content-label tx-11 tx-medium tx-gray-600">Name:</label>
-                <input id="reportCustomer" class="form-control form-control-sm" disabled style="width: 400px">
+                <label class="az-content-label tx-11 tx-medium tx-gray-600">From</label>
+                <input type="date" id="reportCustomer" class="form-control form-control-sm">
               </div>
             </div>
-          </div>
-          <div class="col-sm-6 col-md-4">
-            <label><strong>Date:</strong></label>
-            <div class="row mg-t-10">
-              <div class="col-sm-4">
-                <div class="form-group mb-2">
-                  <label class="az-content-label tx-11 tx-medium tx-gray-600">Date From</label>
-                  <input type="date" id="reportCustomer" class="form-control form-control-sm">
-                </div>
-              </div>
-              <div class="col-sm-4">
-                <div class="form-group mb-2">
-                  <label class="az-content-label tx-11 tx-medium tx-gray-600">Date To</label>
-                  <input type="date" id="reportCustomer" class="form-control form-control-sm">
-                </div>
+            <div class="col-sm-5">
+              <div class="form-group mb-2">
+                <label class="az-content-label tx-11 tx-medium tx-gray-600">To</label>
+                <input type="date" id="reportCustomer" class="form-control form-control-sm">
               </div>
             </div>
           </div>
         </div>
-      </div><!-- card-body -->
+        <div class="col-sm-2 col-md-1">
+          <br>
+          <button id="monthlySalesRunBtn" type="button" class="btn btn-primary btn-sm">Run</button>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-sm-12 col-md-9">
+          <table class="table table-sm">
+            <thead>
+              <tr>
+                <th>Code</th>
+                <th>Item Name</th>
+                <th>Unit</th>
+                <th>Freq</th>
+                <th>Quantity</th>
+                <th>Avg Price Ugx</th>
+                <th>Amount Ugx</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
     </div><!-- card -->
   </div><!-- az-content-body -->
   <?= $this->endSection() ?>
@@ -67,3 +64,10 @@
   <!-- <script src="<?= "" //base_url('assets/scripts/sales.js') 
                     ?>"></script> -->
   <?= $this->endSection() ?>
+
+  <!-- SELECT grade_code AS code, grade_name AS item, unit, count(grade_code) AS transactions, sum(qty_out) AS qty, sum(price*exch_rate*qty_out) AS value
+FROM inventory
+JOIN grades USING (grade_id)
+WHERE transaction_type_id=2 AND client_id=15
+GROUP BY grade_id
+ORDER BY grade_id -->
